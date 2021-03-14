41a4ac203b791ce400dad471105f898e7fbf3db1
Fadi Elsayed <fadi.fgf@gmail.com>
2021-03-14 11:35:26 +0200

    Initial commit

---
+++ android/app/src/main/java/com/matador12/my1nikeonlinestoreyui/MainActivity.java
@@ -0,0 +1,6 @@
+package com.matador12.my1nikeonlinestoreyui;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/constants.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+Color kPurpleCustom = Color(0xFF1E0558);
+Color kDiscountColor = Color(0xFFEF5B5C);
+
